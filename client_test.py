5437969aa7ceea22e7d27f7cfbbfb15ffd421bc5
Harsh <tripathiharsh16@gmail.com>
2023-07-06 19:18:39 +0530

    Client Script file fixed and one unit test-case added

--- client_test.py
+++ client_test.py
@@ -18,7 +18,11 @@ class ClientTest(unittest.TestCase):
 
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getDataPoint_calculatePriceBidEqualToAsk(self):
+    quotes = [
+      {'top_ask': {'price': 117.5, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.5, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 119.2, 'size': 109}, 'id': '0.109974697771', 'stock': 'DEF'},
+    ]
 
 
 if __name__ == '__main__':
